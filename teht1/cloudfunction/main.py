038c991503d0e6f6aa02fb2037d583b0eca7e1dc
Henna <henna.haapala@gmail.com>
2021-12-17 09:48:54 +0200

    kissaweb testi 3

--- teht1/cloudfunction/main.py
+++ teht1/cloudfunction/main.py
@@ -1,3 +1,10 @@
 def kissaweb_subscription(request):
-    #tänne requestin määrittely
+
+    # if request.args and 'nimi' in request.args:
+    #     name = int(request.args.get('nimi'))
+    # elif request_json and 'length' in request_json:
+    #     name = int(request_json['nimi'])
+    # else:
+    #     name = "Juukeli"
+
     return "Thank you for subscribing to cat facts!"
