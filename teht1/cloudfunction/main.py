82c285eb7ac62af917389f695b809af845102bfd
Henna <henna.haapala@gmail.com>
2021-12-17 09:56:27 +0200

    catfacts demo

--- teht1/cloudfunction/main.py
+++ teht1/cloudfunction/main.py
@@ -1,10 +1,12 @@
 def kissaweb_subscription(request):
 
-    # if request.args and 'nimi' in request.args:
-    #     name = int(request.args.get('nimi'))
-    # elif request_json and 'length' in request_json:
-    #     name = int(request_json['nimi'])
-    # else:
-    #     name = "Juukeli"
+    request_json = request.get_json()
+    
+    if request.args and 'nimi' in request.args:
+        name = int(request.args.get('nimi'))
+    elif request_json and 'length' in request_json:
+        name = int(request_json['nimi'])
+    else:
+         name = "Juukeli"
 
-    return "Thank you for subscribing to cat facts!"
+    return f"Thank you for subscribing to cat facts, {name}!"
